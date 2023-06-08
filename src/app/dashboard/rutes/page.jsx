84c3d81872915bac5e9b5e42e32da31a0c5c5e0e
jonathan <zlatanjon@hotmail.com>
2023-06-07 23:19:46 -0600

    Feat(Firebase): Install firebase

--- src/app/dashboard/rutes/page.jsx
+++ src/app/dashboard/rutes/page.jsx
@@ -1,8 +1,37 @@
+'use client';
+import Maps from "@/components/Maps";
+import Image from "next/image";
 
 const Rutes = () => {
   return (
     <div className="container mx-auto px-4 h-screen">
-      <h1 className="text-4xl font-bold">Rutes</h1>
+
+      <div className="grid grid-cols-2 gap-4 p-2">
+        <div>
+          <Image
+            src="/rutes_logo.png"
+            alt="Rutes"
+            width={150}
+            height={70}
+            priority
+          />
+        </div>
+        <div className="flex justify-end items-center gap-4">
+          <span>Filtrar rutas</span>
+          <button className="bg-yellow text-white px-4 py-2 rounded-md">Nueva ruta</button>
+        </div>
+      </div>
+      <div className="grid grid-rows-1 gap-4">
+        <Maps />
+      </div>
+      <div className="grid grid-cols-3 gap-4">
+        <div>
+          <span>Notificaciones</span>
+        </div>
+        <div className="col-span-2">
+          <span>Rutas</span>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Maps.jsx
@@ -0,0 +1,10 @@
+
+const Maps = () => {
+  return (
+    <div className="bg-gray lg:h-[350px] sm:h-[250px]">
+      <h1>Maps</h1>
+    </div>
+  );
+}
+
+export default Maps;
