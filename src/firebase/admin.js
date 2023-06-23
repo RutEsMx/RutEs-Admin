2cbbff56adef3c18c5d43f2a84b557994454b781
jonathan <zlatanjon@hotmail.com>
2023-06-22 22:31:55 -0600

    fix(firebase-admin): Se modifican variables para el initilizeApp

--- src/firebase/admin.js
+++ src/firebase/admin.js
@@ -1,27 +1,26 @@
 var admin = require("firebase-admin");
 
-admin.initializeApp({
-  credential: admin.credential.cert({
-    private_key: JSON.parse(process.env.FIREBASE_PRIVATE_KEY),
-    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-    client_Email: process.env.FIREBASE_CLIENT_EMAIL,
-    client_id: process.env.FIREBASE_CLIENT_ID,
-    project_id: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    type: "service_account",
-    auth_uri: "https://accounts.google.com/o/oauth2/auth",
-    token_uri: "https://oauth2.googleapis.com/token",
-    auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-    client_x509_cert_url: "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-mhbzn%40rutesmx.iam.gserviceaccount.com",
-    universe_domain: "googleapis.com"
-  })
-});
-// if(!admin.apps.length) {
-//   try {
-    
-//   } catch (error) {
-//     console.log(error);
-//   }
-// }
+if(!admin.apps.length) {
+  try {
+    admin.initializeApp({
+      credential: admin.credential.cert({
+        private_key: JSON.parse(process.env.FIREBASE_ADMIN_PRIVATE_KEY),
+        private_key_id: process.env.FIREBASE_ADMIN_PRIVATE_KEY_ID,
+        client_email: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
+        client_id: process.env.FIREBASE_ADMIN_CLIENT_ID,
+        project_id: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+        type: "service_account",
+        auth_uri: "https://accounts.google.com/o/oauth2/auth",
+        token_uri: "https://oauth2.googleapis.com/token",
+        auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+        client_x509_cert_url: "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-mhbzn%40rutesmx.iam.gserviceaccount.com",
+        universe_domain: "googleapis.com"
+      })
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 
 export const firestore = admin.firestore();
