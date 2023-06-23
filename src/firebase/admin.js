55e88148470aa7fb3667207b1476c9b0d1a7154a
jonathan <zlatanjon@hotmail.com>
2023-06-22 22:18:53 -0600

    fix(firebase-admin): Se modifican variables para el initilizeApp

--- src/firebase/admin.js
+++ src/firebase/admin.js
@@ -1,7 +1,5 @@
 var admin = require("firebase-admin");
 
-var serviceAccount = require("./serviceAccountKey.json");
-
 if(!admin.apps.length) {
   try {
     admin.initializeApp({
@@ -11,7 +9,12 @@ if(!admin.apps.length) {
         client_Email: process.env.FIREBASE_CLIENT_EMAIL,
         client_id: process.env.FIREBASE_CLIENT_ID,
         project_id: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-        ...serviceAccount
+        type: "service_account",
+        auth_uri: "https://accounts.google.com/o/oauth2/auth",
+        token_uri: "https://oauth2.googleapis.com/token",
+        auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+        client_x509_cert_url: "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-mhbzn%40rutesmx.iam.gserviceaccount.com",
+        universe_domain: "googleapis.com"
       })
     });
   } catch (error) {
