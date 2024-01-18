0133f42594591b56cbe25cfd35d598865d7964a0
jonathan <zlatanjon@hotmail.com>
2024-01-17 22:33:27 -0600

    :sparkles:(PasswordInput): Add password visibility toggle to InputField component

--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -1,4 +1,6 @@
 import { Field } from "formik";
+import { EyeIcon, EyeSlashIcon } from "@heroicons/react/24/outline";
+import { useState } from "react";
 
 const InputField = ({
   label,
@@ -10,19 +12,39 @@ const InputField = ({
   className,
   ...props
 }) => {
+  const [showPassword, setShowPassword] = useState(false);
   return (
     <div className="flex flex-col text-xs m-2">
       <label htmlFor={name} className="mb-1">
         {label}
       </label>
-      <Field
-        name={name}
-        type={type}
-        value={value}
-        onChange={onChange}
-        className={`h-8 border border-gray rounded-sm p-1 ${className || ''}`}
-        {...props}
-      />
+      <div className="flex flex-row gap-2">
+        <Field
+          name={name}
+          type={type === "password" && showPassword ? "text" : type}
+          value={value}
+          onChange={onChange}
+          className={`h-8 border border-gray rounded-sm p-1 w-full ${
+            className || ""
+          }`}
+          {...props}
+        />
+        {type === "password" ? (
+          <div className="w-8 h-8 flex items-center justify-center">
+            {showPassword ? (
+              <EyeSlashIcon
+                onClick={() => setShowPassword(false)}
+                className="h-6 w-6 text-gray-500"
+              />
+            ) : (
+              <EyeIcon
+                onClick={() => setShowPassword(true)}
+                className="h-6 w-6 text-gray-500"
+              />
+            )}
+          </div>
+        ) : null}
+      </div>
       {error && <span className="text-red">{error}</span>}
     </div>
   );
