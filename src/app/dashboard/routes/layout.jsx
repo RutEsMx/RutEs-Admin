2caa9f561f073a096c9842c0e77d13c4bb97ecf2
jonathan <zlatanjon@hotmail.com>
2026-03-03 17:50:17 -0600

    Remove unnecessary margin from layout components

--- src/app/dashboard/routes/layout.jsx
+++ src/app/dashboard/routes/layout.jsx
@@ -1,3 +1,3 @@
 export default function Layout({ children }) {
-  return <div className="col-span-3 lg:col-span-4 mt-16 mx-8">{children}</div>;
+  return <div className="col-span-3 lg:col-span-4 mx-8">{children}</div>;
 }
